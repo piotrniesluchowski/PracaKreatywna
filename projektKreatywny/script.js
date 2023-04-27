3b629eb2714b5cf8332421c9e799a918945abaf4
piotrniesluchowski <73973777+piotrniesluchowski@users.noreply.github.com>
2023-04-27 21:02:05 +0200

    Delete projektKreatywny directory

--- projektKreatywny/script.js
+++
@@ -1,8 +0,0 @@
-let cont = document.querySelector('container');
-let layer = document.querySelector('layer');
-
-cont.onscroll = function(){
-    let x = cont.scrollTop;
-
-    layer[0].style.top = x + 'px';
-}
